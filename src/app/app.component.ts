99753e39e00d23e47858d300fdf361990e6d840b
Marwa Mahmoud <mm3764453@gmail.com>
2024-07-11 10:45:19 +0300

    Implemented displaying customers along with their transaction data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CustomerTransactionTableComponent } from './components/customer-transaction-table/customer-transaction-table.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CustomerTransactionTableComponent],
+  imports: [RouterOutlet, CustomerTransactionTableComponent, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'customer-transaction-app';
