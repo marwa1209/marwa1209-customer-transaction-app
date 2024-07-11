3815d0aa9702480800c6186cb8abf2ee80af7ab0
Marwa Mahmoud <mm3764453@gmail.com>
2024-07-11 08:09:50 +0300

    Create interfaces for Customer and Transaction

---
+++ src/app/components/customer-transaction-table/customer-transaction-table.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+@Component({
+  selector: 'app-customer-transaction-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-transaction-table.component.html',
+  styleUrl: './customer-transaction-table.component.scss'
+})
+export class CustomerTransactionTableComponent {
+
+}
---
+++ src/app/interfaces/transaction.ts
@@ -0,0 +1,6 @@
+export interface Transaction {
+  id: number;
+  customer_id: number;
+  date: string;
+  amount: number;
+}
