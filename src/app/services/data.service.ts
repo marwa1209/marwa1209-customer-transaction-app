0bbfdbf28a2dd0a974d4636bb247c168ff1aac7c
Marwa Mahmoud <mm3764453@gmail.com>
2024-07-14 01:54:46 +0300

    add api url

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class DataService {
-  private apiUrl = 'http://localhost:3001';
+  private apiUrl = 'https://transaction-app-server-beta.vercel.app';
 
   constructor(private http: HttpClient) {}
 
