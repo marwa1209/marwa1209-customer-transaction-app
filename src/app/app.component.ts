c13590dda6b38e4fd1ae8f400ca58fcaa418f79f
Marwa Mahmoud <mm3764453@gmail.com>
2024-07-14 02:07:16 +0300

    edit responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CustomerTransactionTableComponent } from './components/customer-transaction-table/customer-transaction-table.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from "./components/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CustomerTransactionTableComponent, HttpClientModule],
+  imports: [RouterOutlet, CustomerTransactionTableComponent, HttpClientModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
